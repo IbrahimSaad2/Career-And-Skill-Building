36c154a299e1b4fe0f77961dac0d8b381b3ffff1
IbrahimSaad <himasaad187@gmail.com>
2025-05-29 17:47:33 +0300

    dashboard job sekeer

--- src/app/Components/job-seeker-dashboard/job-seeker-dashboard.component.ts
+++ src/app/Components/job-seeker-dashboard/job-seeker-dashboard.component.ts
@@ -1,13 +1,74 @@
-import { Component } from '@angular/core';
+import { Component,ViewChild } from '@angular/core';
 import { routes } from '../../app.routes';
 import { RouterLink } from '@angular/router';
+import {   ApexChart,
+  ApexNonAxisChartSeries,
+  ApexPlotOptions,
+  ApexFill,
+  ApexStroke,
+  ApexDataLabels, 
+  NgApexchartsModule} from "ng-apexcharts";
+
+export type ChartOptions = {
+  series: ApexNonAxisChartSeries;
+  chart: ApexChart;
+  plotOptions: ApexPlotOptions;
+  fill: ApexFill;
+  stroke: ApexStroke;
+  labels: string[];
+  dataLabels: ApexDataLabels;
+};
+
 
 @Component({
   selector: 'app-job-seeker-dashboard',
-  imports: [RouterLink],
+  imports: [RouterLink, NgApexchartsModule],
   templateUrl: './job-seeker-dashboard.component.html',
   styleUrl: './job-seeker-dashboard.component.css'
 })
 export class JobSeekerDashboardComponent {
+ user = {
+  name:"Ibrahim Saad",
+  JobTitle:"FrontEnd Developer",
+  EducationLevel:"Foruth"
+ }
 
+  chartOptions: any = {
+    series: [75],
+    chart: {
+      height: 250,
+      type: 'radialBar'
+    },
+    plotOptions: {
+      radialBar: {
+        hollow: {
+          size: '60%'
+        },
+        dataLabels: {
+          name: {
+            show: true,
+            offsetY: -10,
+            fontSize: '18px',
+            fontWeight: 'bold',
+            color: '#000'
+          },
+          value: {
+            fontSize: '20px',
+            fontWeight: 'bold',
+            color: '#000',
+            show: true,
+          }
+        }
+      }
+    },
+    fill: {
+      colors: ['#009688']
+    },
+    stroke: {
+      lineCap: 'round'
+    },
+    labels: ['Complete']
+  };
+  
+  
 }
