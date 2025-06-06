3510775ab15c2c8f3ab880f053352d5472cd5857
IbrahimSaad <himasaad187@gmail.com>
2025-06-06 05:12:52 +0300

    some change

--- src/app/Components/login-employer/login-employer.component.ts
+++ src/app/Components/login-employer/login-employer.component.ts
@@ -42,7 +42,6 @@ export class LoginEmployerComponent {
             localStorage.setItem('token',res.token);
             localStorage.setItem('details', JSON.stringify(res));
             this._Router.navigate(['../../employer/home']);
-          
         },
         error:(err)=>{
           console.log(err)
